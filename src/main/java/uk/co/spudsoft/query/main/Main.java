14401388637041d0d465caaae39fc47eb9fd070c
Yaytay <jim-github@spudsoft.co.uk>
2023-02-14 11:18:39 +0000

    Get the UI repo using maven-scm-plugin

--- src/main/java/uk/co/spudsoft/query/main/Main.java
+++ src/main/java/uk/co/spudsoft/query/main/Main.java
@@ -109,7 +109,7 @@ public class Main extends Application {
   private static final Logger logger = LoggerFactory.getLogger(Main.class);
   
 private static final String MAVEN_PROJECT_NAME = "SpudSoft Query Engine";
-private static final String MAVEN_PROJECT_VERSION = "0.0.0-3-main-SNAPSHOT";
+private static final String MAVEN_PROJECT_VERSION = "0.0.0-4-main-SNAPSHOT";
 
 private static final String NAME = "query-engine";
   
