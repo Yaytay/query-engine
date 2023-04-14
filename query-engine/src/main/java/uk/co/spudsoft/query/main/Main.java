981501eb2abe4e76d4ffd79d101a20c00c70364e
Yaytay <jim-github@spudsoft.co.uk>
2023-04-14 16:11:28 +0100

    Skip site plugin on parent

--- query-engine/src/main/java/uk/co/spudsoft/query/main/Main.java
+++ query-engine/src/main/java/uk/co/spudsoft/query/main/Main.java
@@ -108,7 +108,7 @@ public class Main extends Application {
   private static final Logger logger = LoggerFactory.getLogger(Main.class);
   
 private static final String MAVEN_PROJECT_NAME = "SpudSoft Query Engine";
-private static final String MAVEN_PROJECT_VERSION = "0.0.1-1-design-SNAPSHOT";
+private static final String MAVEN_PROJECT_VERSION = "0.0.1-2-design-SNAPSHOT";
 
 private static final String NAME = "query-engine";
   
