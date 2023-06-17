eed99ff0e0817d7452ab2f8366390e96af76347a
Yaytay <jim-github@spudsoft.co.uk>
2023-06-17 06:31:12 +0100

    Correct licence header in Version.java

--- query-engine/src/main/java/uk/co/spudsoft/query/main/Version.java
+++ query-engine/src/main/java/uk/co/spudsoft/query/main/Version.java
@@ -1,7 +1,7 @@
 package uk.co.spudsoft.query.main;
 
 /*
- * Copyright (C) 2023 njt
+ * Copyright (C) 2023 jtalbut
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
