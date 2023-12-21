de9e16f9fa677c1b80eb72fbe63ae03ca4c2279c
Yaytay <jim-gitkraken@spudsoft.co.uk>
2023-12-21 18:22:09 +0000

    SpudSoft dependency bumps

--- query-engine/src/main/java/uk/co/spudsoft/query/main/Version.java
+++ query-engine/src/main/java/uk/co/spudsoft/query/main/Version.java
@@ -25,7 +25,7 @@ package uk.co.spudsoft.query.main;
 public final class Version {
 
   public static final String MAVEN_PROJECT_NAME = "SpudSoft Query Engine";
-  public static final String MAVEN_PROJECT_VERSION = "0.0.22-42-main";
+  public static final String MAVEN_PROJECT_VERSION = "0.0.22-43-main";
 
   private Version() {
   }
