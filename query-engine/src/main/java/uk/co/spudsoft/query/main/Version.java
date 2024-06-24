5964adeb50440619a6f2e756a681d874b3c5b10a
Jim Talbut <jim.talbut@groupgti.com>
2024-06-24 09:15:49 +0100

    Improve sample compose file and tweak docs to match

--- query-engine/src/main/java/uk/co/spudsoft/query/main/Version.java
+++ query-engine/src/main/java/uk/co/spudsoft/query/main/Version.java
@@ -34,7 +34,7 @@ public final class Version {
   /**
    * The project version, as set in the Maven pom.xml.
    */
-  public static final String MAVEN_PROJECT_VERSION = "0.0.36-3-main";
+  public static final String MAVEN_PROJECT_VERSION = "0.0.37-main";
 
   private Version() {
   }
